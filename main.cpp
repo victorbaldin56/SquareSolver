8d6216f2057314cb04d3335842138325436831b6
victor <victor@MiWiFi-R2350-srv.lan>
2023-08-15 22:51:24 +0300

    add named constants to main.cpp

--- main.cpp
+++ main.cpp
@@ -10,16 +10,16 @@ int main() {
     printf("Enter equation coeffitients: ");
     scanf("%lf %lf %lf", &a, &b, &c);
     switch(solver(roots, a, b, c)) {
-        case 0:
+        case NORMAL:
             if (roots[0] != roots[1])
                 printf("%g %g\n", roots[0], roots[1]);
             else
                 printf("%g\n", roots[0]);
             break;
-        case 1:
+        case NOSOL:
             printf("Error: no solution in real numbers\n");
             break;
-        case 2:
+        case NOTSQUARE:
             printf("Error: not square equation\n");
             break;
     }
