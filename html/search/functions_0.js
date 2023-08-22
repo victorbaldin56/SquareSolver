681c5a895f3605bc238839b17a3a741a0694576d
victor <victor@MiWiFi-R2350-srv.lan>
2023-08-22 18:07:19 +0300

    docs fixes

---
+++ html/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['isequal_0',['isequal',['../solve_8cpp.html#a69e45bdefacf24536533958e9475eeb3',1,'isequal(double a, double b):&#160;solve.cpp'],['../solver_8h.html#a69e45bdefacf24536533958e9475eeb3',1,'isequal(double a, double b):&#160;solve.cpp']]]
+];
---
+++ html/search/variables_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['infinite_0',['INFINITE',['../solver_8h.html#ae831e56053b8abc343b931bc11a63be4',1,'solver.h']]]
+];
