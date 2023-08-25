bbab4a523014f31720f56c5e2d5cad5e460ea8cf
victor <victor@MiWiFi-R2350-srv.lan>
2023-08-25 15:25:55 +0300

    fixes

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]) {
     printf("# Square equation solver\n"
            "# (c) Victor Baldin, 2023\n");
     
-    #ifdef DEBUG
+    #ifndef NDEBUG
     if (argc == 3 && !strncmp("--test", argv[1], MAXLEN)) { 
         // run tests
         printf("# Running test\n");
